b75346b854492d11273348bd14789ecb0bcc7256
jplumb <jplumb@newrelic.com>
2020-06-08 16:08:01 -0700

    add infinite

--- src/main/java/com/newrelic/ex/Example01_Lazy.java
+++ src/main/java/com/newrelic/ex/Example01_Lazy.java
@@ -3,8 +3,10 @@ package com.newrelic.ex;
 import com.newrelic.model.Burger;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static com.newrelic.model.BurgerBackoffice.dailyOrders;
 
@@ -17,10 +19,22 @@ public class Example01_Lazy {
 
     public static void main(String[] args) {
         System.out.println("Starting");
+        Example01_Lazy ex = new Example01_Lazy();
+        ex.lazy();
+        ex.infinite();
+        System.out.println("Program complete");
+    }
+
+    private void lazy() {
         IntStream.range(10, 20)
-            .map(operand -> {
-                System.out.println("Processing " + operand);
-                return 0;
-            });
+                .map(operand -> {
+                    System.out.println("Processing " + operand);
+                    return 0;
+                });
+    }
+
+    private void infinite() {
+        AtomicLong value = new AtomicLong();
+        Stream.generate(value::getAndIncrement);
     }
 }
